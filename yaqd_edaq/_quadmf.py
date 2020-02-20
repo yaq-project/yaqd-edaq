c5bbbf071495eaf58ffe3628c33c30aa3b4c1381
Louis Morris <lvmorris@wisc.edu>
2020-02-19 19:52:55 -0600

    fixed _init_ and _measure, also added blink

--- yaqd_edaq/_quadmf.py
+++ yaqd_edaq/_quadmf.py
@@ -23,7 +23,7 @@ class QuadMF(Sensor):
             mode = config[f'c_{i}_mode']
             self.ser.write(f"set c {i} function {mode}\n".encode())
             if mode == 'pH&ISE':
-                self.ser.write(f"set c {i} units config[f'c_{i}_units']\n".encode())
+                self.ser.write(f"set c {i} units {config[f'c_{i}_units']}\n".encode())
             
         self.channels = {"channel 1": (config.get('c_1_units',None), ()),
                          "channel 2": (config.get('c_2_units',None), ()),
@@ -32,42 +32,21 @@ class QuadMF(Sensor):
                          }
 
 
-    def _load_state(self, state):
-        """Load an initial state from a dictionary (typically read from the state.toml file).
-
-        Must be tolerant of missing fields, including entirely empty initial states.
-
-        Parameters
-        ----------
-        state: dict
-            The saved state to load.
-        """
-        super()._load_state(state)
-        # This is an example to show the symetry between load and get
-        # If no persistent state is needed, these unctions can be deleted
-        self.value = state.get("value", 0)
-
-    def get_state(self):
-        state = super().get_state()
-        state["value"] = self.value
-        return state
-
-
-
+    def blink(self):
+        self.ser.write(b"blink\n")
+        self.ser.write(b"blink\n")
+  
     async def _measure(self):
-        import numpy
-        return {"channel": 0, "curtis": numpy.pi}
-
-
-
-    async def update_state(self):
-        """Continually monitor and update the current daemon state."""
-        # If there is no state to monitor continuously, delete this function
-        while True:
-            # Perform any updates to internal state
-            self._busy = False
-            # There must be at least one `await` in this loop
-            # This one waits for something to trigger the "busy" state
-            # (Setting `self._busy = True)
-            # Otherwise, you can simply `await asyncio.sleep(0.01)`
-            await self._busy_sig.wait()
+        self.ser.reset_input_buffer()
+        self.ser.reset_output_buffer()
+        self.ser.write(b'r\n')
+        raw = self.ser.readline()
+        raw = self.ser.readline()
+        raw = raw.split()
+        vals = []
+        for r in raw:
+            try:
+                vals.append(float(r))
+            except ValueError:
+                vals.append(float("nan"))
+        return {f"channel {i}": float(vals[i*2]) for i in range(1,5)}
